a9240ce1324e796c67afe5f5b09a9789d2ac5b91
Filippo Ghinelli <Filippo Ghinelli@localhost>
2016-03-24 16:57:24 +0100

    proseguimento gui

---
+++ src/view/MyTableModel.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.table.AbstractTableModel;
+
+public class MyTableModel extends AbstractTableModel {
+    
+    private List<String> base = new ArrayList<>();
+    
+    public MyTableModel() {
+        //da usare una lista di oggetti tipo JTextArea o JTextPane per avere una lista completa di oggetti in cui far scrivere poi all'interno di essi
+        base.add("Monday");
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 10;
+    }
+
+    @Override
+    public int getRowCount() {
+        return (1/*riga giorno e orari*/+11/*numero classi*/) * 5;
+    }
+
+    @Override
+    public Object getValueAt(final int rowIndex, final int columnIndex) {
+        //da avere una lista completamente piena da cui pescare
+        return this.base.get(0);
+    }
+    
+    @Override
+    public void setValueAt(Object value, int row, int column) {
+        //lista dei giorni,aule,ore (per ora solo esempio)
+        this.base.set(/*da modificare*/row*column, (String)value);
+        fireTableCellUpdated(row, column);
+    }
+    
+    
+
+}
--- src/view/ScheletonGUI.java
+++ src/view/ScheletonGUI.java
@@ -13,8 +13,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.JToolBar;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.TableColumn;
 
 public class ScheletonGUI extends JFrame {
 
@@ -22,12 +25,12 @@ public class ScheletonGUI extends JFrame {
      * 
      */
     private static final long serialVersionUID = 6251123285697956614L;
-    private JTable table = new JTable(50, 50);
+    private JTable table = new JTable(new MyTableModel());
     private JScrollPane fullTable = new JScrollPane(this.table);
     private JToolBar optionBar = new JToolBar();
     private JButton search = new JButton("Search");
     private JButton edit = new JButton("Edit");
-    private JButton load = new JButton("Load");
+    private JButton open = new JButton("Open");
     private JButton save = new JButton("Save");
     
     ScheletonGUI() {
@@ -38,16 +41,22 @@ public class ScheletonGUI extends JFrame {
         this.optionBar.setFloatable(false);
         this.optionBar.add(this.search);
         this.optionBar.add(this.edit);
-        this.optionBar.add(this.load);
+        this.optionBar.add(this.open);
         this.optionBar.add(this.save);
         
-        this.table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        this.table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); //metterla o toglierla sta opzione? pensarci bene
+        this.table.setTableHeader(null);
+        this.table.setFillsViewportHeight(true); //da usare forse, capire bene di cosa si tratta
+        this.table.setFocusable(false);
+        this.table.setRowSelectionAllowed(false);
+        
+        
         
         JPanel legenda = new JPanel(new GridBagLayout());
         legenda.setBorder(new TitledBorder("Legenda"));
         GridBagConstraints cnst = new GridBagConstraints();
         cnst.gridy = 0;
-        JLabel prova = new JLabel("  ");
+        JLabel prova = new JLabel("  ");//in mezzo ad un for
         prova.setOpaque(true);
         prova.setBackground(Color.BLACK);
         legenda.add(prova, cnst);
