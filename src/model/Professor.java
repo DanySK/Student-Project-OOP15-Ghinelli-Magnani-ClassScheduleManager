bbc8c80a0c936d4c129303b069f315774f67b0de
Martina_Magnani <Martina_Magnani@localhost>
2016-04-01 14:28:21 +0200

    Aggiunta enum ClassRoom e eliminata classe ClassRoom

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -30,8 +30,40 @@ public class Professor {
     public String getProfessor() {
         return this.name + " " + this.surname;
     }
-    
-    public boolean equals(final Professor p){
-        return this.getProfessor().equals(p.getProfessor());
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+        return result;
     }
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Professor other = (Professor) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (surname == null) {
+            if (other.surname != null)
+                return false;
+        } else if (!surname.equals(other.surname))
+            return false;
+        return true;
+    }
+    
 }
--- src/model/Teaching.java
+++ src/model/Teaching.java
@@ -25,8 +25,35 @@ public class Teaching {
     public String getTeaching(){
         return this.name;
     }
-    
-    public boolean equals(final Teaching t){
-        return this.getTeaching().equals(t.getTeaching());
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
     }
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Teaching other = (Teaching) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+    
+    
 }
