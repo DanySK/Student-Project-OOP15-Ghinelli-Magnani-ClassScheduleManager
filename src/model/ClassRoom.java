b7f631035ca7e5ffd656490441641ad69fc4e64c
Martina_Magnani <Martina_Magnani@localhost>
2016-04-01 14:30:04 +0200

    Sistemazione enum classroom

---
+++ src/model/ClassRoom.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Marti
+ *
+ */
+public enum ClassRoom {
+    AULA_MAGANA,
+    AULA_A,
+    AULA_B,
+    AULA_C,
+    AULA_D,
+    AULA_E,
+    AULA_VIA_RASI,
+    LAB_VELA,
+    LAB_2,
+    LAB_3;
+}
