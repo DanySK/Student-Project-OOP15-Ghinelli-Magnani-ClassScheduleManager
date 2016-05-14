4812ee3649b796a6ca1a6e96b684e32a5da41890
Martina_Magnani <Martina_Magnani@localhost>
2016-05-14 18:02:30 +0200

    Modifica classe DataManagerImpl

--- src/controller/IDataManager.java
+++ src/controller/IDataManager.java
@@ -7,5 +7,6 @@ import model.SchedulesModel;
 public interface IDataManager {
     
     void readConfig(final SchedulesModel model) throws IOException;
-
+    public void saveFile(final String fileName, final SchedulesModel model) throws IOException;
+    public void openFile(final String fileName, SchedulesModel model) throws IOException, ClassNotFoundException;
 }
--- src/controller/IDataManegerImpl.java
+++ src/controller/IDataManegerImpl.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,18 +17,50 @@ public class IDataManegerImpl implements IDataManager {
     
     private final Path configPath = FileSystems.getDefault().getPath("res", "config.yml");
 
-    @Override
     public void readConfig(final SchedulesModel model) throws IOException {
         final List<String> contentFile = Files.readAllLines(this.configPath);
         for (final String s : contentFile) {
             final StringTokenizer st = new StringTokenizer(s, ":");
             if (st.countTokens() == 2) {
                 final String temp = st.nextToken();
-                if ("aula".equals(temp)) {
+                if (temp.equals("aula")) {
                     model.addClassroom(st.nextToken());
+                }
+                if (temp.equals("anno")) {
+                    model.addYears(st.nextToken());
+                }
+                if (temp.equals("prof")) {
+                    model.addProfessor(st.nextToken());
                 }
             }
         }
+    }
+    /**
+     * Method to save an object on a file.
+     * 
+     * @param fileName The system-dependent filename.
+     * @param obj Object to be saved on file.
+     * @throws IOException {@link ObjectOutputStream#writeObject(Object)}.
+     * @author Martina Magnani
+     */
+    public void saveFile(final String fileName, final SchedulesModel model) throws IOException {
+            final ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+            oos.writeObject(model);
+            oos.close();
+    }
+    /**
+     * Method to load an object on a file.
+     * 
+     * @param fileName The system-dependent filename.
+     * @return Object loaded from a file.
+     * @throws IOException construct method of {@link ObjectIntputStream}.
+     * @throws ClassNotFoundException {@link ObjectInputStream#readObject()}.
+     * @author Martina Magnani
+     */
+    public void openFile(final String fileName, SchedulesModel model) throws IOException, ClassNotFoundException {
+            final ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+            model = (SchedulesModel) ois.readObject();
+            ois.close();
     }
 
 }
