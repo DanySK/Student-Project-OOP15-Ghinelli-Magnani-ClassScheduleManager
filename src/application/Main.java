30b57347cfa728d93a41415e37f4566a63dbe720
Filippo Ghinelli <Filippo Ghinelli@localhost>
2016-07-04 15:49:43 +0200

    Puliti errori checkstyle nel main

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,11 +3,21 @@ package application;
 import controller.Controller;
 import view.ViewImpl;
 
+/**
+ * Class responsible for stating the entire program.
+ *
+ */
+
 public final class Main {
     
     private Main() {
         
     }
+
+    /**
+     * Main method which starts the program.
+     * @param args args.
+     */
     
     public static void main(final String... args) {
         Controller.getController().setView(new ViewImpl());
